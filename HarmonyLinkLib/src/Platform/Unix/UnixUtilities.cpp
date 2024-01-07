1214f1d33f91494f05882c2f8a5b254366fc9765
Jordon Brooks <jordonbc@hotmail.co.uk>
2024-01-07 13:54:43 +0000

    stuff

--- HarmonyLinkLib/src/Platform/Unix/UnixUtilities.cpp
+++ HarmonyLinkLib/src/Platform/Unix/UnixUtilities.cpp
@@ -4,3 +4,18 @@ bool UnixUtilities::is_running_under_wine() {
     // Unix/Linux specific implementation
     return false;
 }
+
+std::shared_ptr<FCPUInfo> UnixUtilities::get_cpu_info()
+{
+    return nullptr;
+}
+
+std::shared_ptr<FBattery> UnixUtilities::get_battery_status()
+{
+    return nullptr;
+}
+
+std::shared_ptr<FOSVerInfo> UnixUtilities::get_os_version()
+{
+    return nullptr;
+}
--- HarmonyLinkLib/src/Platform/Unix/UnixUtilities.h
+++ HarmonyLinkLib/src/Platform/Unix/UnixUtilities.h
@@ -5,6 +5,11 @@
 class UnixUtilities : public IPlatformUtilities {
 public:
     bool is_running_under_wine() override;
+
+    std::shared_ptr<FCPUInfo> get_cpu_info() override;
+    std::shared_ptr<FBattery> get_battery_status() override;
+    std::shared_ptr<FOSVerInfo> get_os_version() override;
     
+     override
     // Implementation for other Unix/Linux-specific functions
 };
